f4b76563c39055dc48bde268847206fef4578b3f
Scott John <scott@thejohnweb.com>
2023-10-24 09:29:37 -0400

    updated updaters

--- updateCopyQ.bat
+++ updateCopyQ.bat
@@ -1,6 +1,6 @@
 @echo off
 pushd %TEMP%
 curl -s https://api.github.com/repos/hluk/CopyQ/releases/latest | grep "browser_download_url.*exe" | cut -d : -f 2,3 | sed -e "s/^[ \t]*//" -e s/^.// -e s/.$// | wget -i -
-FOR /F %A IN ('ls copyq*.exe') DO start /wait %~A /SP- /SILENT /ALLUSERS /INFFILE=D:\users\johns\NextCloud\Scripts\copyqinstall.inf
+FOR /F %A IN ('ls copyq*.exe') DO start /wait %~A /INFFILE=$HOME\NextCloud\Scripts\copyqinstall.inf
 del -y copyq*.exe
 popd
